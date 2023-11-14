9c656839130a0daa9a23ed2ee17407a1a75c1735
KevinH <khui98@gmail.com>
2023-11-14 14:08:26 -0500

    Updated views.py and urls.py
    Added contact.html and about.html

--- job_application/views.py
+++ job_application/views.py
@@ -23,3 +23,9 @@ def index(request):
 
             messages.success(request, "Form submitted successfully!")
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
